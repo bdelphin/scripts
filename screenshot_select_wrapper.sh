4c3b22f840c03065f370161172ef5a1c3ae53695
Baptiste Delphin <baptistedelphin@gmail.com>
2020-04-23 19:13:15 +0200

    minor changes

--- screenshot_select_wrapper.sh
+++ screenshot_select_wrapper.sh
@@ -1,4 +1,6 @@
 #/bin/bash
 
 export DISPLAY=:0; bash -c "/home/baptiste/scripts/screenshot_select.sh"
+
+
 #su - baptiste -c 'export DISPLAY=:0; bash -c "/home/baptiste/scripts/screenshot_select.sh"'
