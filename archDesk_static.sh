3c649b1d69340357749da5d74b2855d52eefbed9
Baptiste DELPHIN <baptiste.delphin@gmail.com>
2021-03-21 14:23:35 +0100

    archDesk static IP script

---
+++ archDesk_static.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Bring interface UP
+ip link set enp4s0 up
+
+# Assign IP
+ip addr add 192.168.1.13/24 dev enp4s0
+
+# Set GW
+ip route add default via 192.168.1.1 dev enp4s0
+
+# Set DNS ?
+
+echo "Network online."
