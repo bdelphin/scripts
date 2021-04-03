64889e7681d0d4a558ddfe65d60c7e0973c29906
Baptiste DELPHIN <baptiste.delphin@gmail.com>
2021-04-03 19:39:24 +0200

    static IP service

--- archDesk_static.sh
+++ archDesk_static.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 # Bring interface UP
-ip link set enp4s0 up
+ip link set enp5s0 up
 
 # Assign IP
-ip addr add 192.168.1.13/24 dev enp4s0
+ip addr add 192.168.1.13/24 dev enp5s0
 
 # Set GW
-ip route add default via 192.168.1.1 dev enp4s0
+ip route add default via 192.168.1.1 dev enp5s0
 
 # Set DNS ?
 
