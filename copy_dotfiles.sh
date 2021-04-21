54078ca50f019aeed0d7482eea83640ea4764f0f
Baptiste DELPHIN <baptiste.delphin@gmail.com>
2021-04-21 19:04:42 +0200

    Script to automatically copy updated dotfiles to my git repo

--- copy_dotfiles.sh
+++ copy_dotfiles.sh
@@ -4,6 +4,13 @@
 
 repo="$HOME/Projects/dotfiles_v2/"
 
+# System
+cp $HOME/.Xresources $repo
+cp $HOME/.bash_profile $repo
+cp $HOME/.bashrc $repo
+cp $HOME/.xinitrc $repo
+cp $HOME/.vimrc $repo
+
 # Polybar
 cp -r $HOME/.config/polybar/* $repo.config/polybar/
 
@@ -22,6 +29,9 @@ cp -r $HOME/.config/discocss/* $repo.config/discocss/
 # Firefox
 profile="$(ls -alh $HOME/.mozilla/firefox/ | grep default-release | awk '{print $NF}')"
 cp -r $HOME/.mozilla/firefox/$profile/chrome/* $repo.mozilla/firefox/XXXXX.default-release/chrome/
+
+# GLava
+cp -r $HOME/.config/glava/* $repo.config/glava/
 
 echo "Dotfiles copied, ready to commit/push."
 
